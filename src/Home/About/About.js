9f222866ffce79bb09c13b6c0628413c59bba05c
ShahanazAhmedNishi <asahanaz207@gmail.com>
2022-12-29 14:48:27 +0600

    about task

--- src/Home/About/About.js
+++ src/Home/About/About.js
@@ -1,9 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
-import EditModal from "../EditModal/EditModal";
 
 const About = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
   const menuItems = (
     <React.Fragment>
       <li className="italic font-serif">
@@ -24,7 +30,11 @@ const About = () => {
     </React.Fragment>
   );
 
-  const { data: about = [], isLoading } = useQuery({
+  const {
+    data: about = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["about"],
     queryFn: async () => {
       const res = await fetch("http://localhost:5000/about");
@@ -35,6 +45,43 @@ const About = () => {
   if (isLoading) {
     return <progress className="progress w-56"></progress>;
   }
+  const id = about[0]._id;
+  const name = about[0].name;
+  const email = about[0].email;
+  const university = about[0].university;
+  const address = about[0].address;
+  // console.log("id", id, name, email, university, address);
+
+  const handleAbout = (data, name, email, university, address) => {
+    console.log(data);
+    console.log(data.name);
+    fetch(`http://localhost:5000/about/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: data.name,
+        email: data.email,
+        university: data.university,
+        address: data.address,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          name = data.name;
+          email = data.email;
+          university = data.university;
+          address = data.address;
+          toast.success("Successfully Updated");
+          refetch();
+          // console.log(name);
+        }
+      });
+  };
+
   return (
     <div className="text-center">
       <div className="navbar flex mx-auto bg-black text-white lg:pl-52">
@@ -83,11 +130,84 @@ const About = () => {
         />
       </div>
       <h2 className="font-bold text-2xl font-serif italic">About</h2>
-      <h2>Name: {about[0].name}</h2>
-      <h2>Email: {about[0].email}</h2>
-      <h2>University: {about[0].university}</h2>
-      <h2>Address: {about[0].address}</h2>
-      <EditModal></EditModal>
+      <h2>Name: {name}</h2>
+      <h2>Email: {email}</h2>
+      <h2>University: {university}</h2>
+      <h2>Address: {address}</h2>
+      {/* <EditModal></EditModal> */}
+      {/* Modal */}
+      <div>
+        <>
+          <input type="checkbox" id="booking-modal" className="modal-toggle" />
+          <div className="modal">
+            <div className="modal-box">
+              <label
+                htmlFor="booking-modal"
+                className="btn btn-sm btn-circle absolute right-2 top-2"
+              >
+                ✕
+              </label>
+              <h3 className="text-lg font-bold mb-3">Update About</h3>
+              <form onSubmit={handleSubmit(handleAbout)}>
+                <input
+                  type="name"
+                  {...register("name", {
+                    required: "Name is required",
+                  })}
+                  placeholder="Name"
+                  className="input input-bordered w-full mb-3"
+                />
+                {errors.name && (
+                  <p className="text-red-600">{errors.name?.message}</p>
+                )}
+                <input
+                  type="email"
+                  {...register("email", {
+                    required: "Email is required",
+                  })}
+                  placeholder="Email"
+                  className="input input-bordered w-full mb-3"
+                />
+                {errors.email && (
+                  <p className="text-red-600">{errors.email?.message}</p>
+                )}
+                <input
+                  type=""
+                  {...register("university", {
+                    required: "University Name is required",
+                  })}
+                  placeholder="University Name"
+                  className="input input-bordered w-full mb-3"
+                />
+                {errors.university && (
+                  <p className="text-red-600">{errors.university?.message}</p>
+                )}
+                <input
+                  type=""
+                  {...register("address", {
+                    required: "Address is required",
+                  })}
+                  placeholder="Address"
+                  className="input input-bordered w-full mb-3"
+                />
+                {errors.address && (
+                  <p className="text-red-600">{errors.address?.message}</p>
+                )}
+                <br />
+                <input
+                  className="w-full btn btn-black"
+                  type="submit"
+                  value="Save"
+                  htmlFor="booking-modal"
+                />
+                {/* <button className="w-full btn btn-black" type="submit">
+                   Edit
+                </button> */}
+              </form>
+            </div>
+          </div>
+        </>
+      </div>
     </div>
   );
 };
